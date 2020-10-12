b79be3c355b967970214795299ddc50c4bcae0f3
ydy <1053144764@qq.com>
2020-10-12 19:34:51 +0800

    the original version

---
+++ sso/app2/src/main/java/cas/client/filter/bFilter.java
@@ -0,0 +1,124 @@
+package cas.client.filter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import cas.client.Constants;
+
+/**
+ * Servlet Filter implementation class SingleSignOnFilter
+ */
+@WebFilter(filterName="F2",urlPatterns="/*")
+public class bFilter implements Filter {
+	private String CAS_LOGIN_URL="http://localhost:8080/cas/login.do";
+	private String CAS_USER_URL="http://localhost:8080/cas/getUser.do";
+
+	/**
+	 * Default constructor.
+	 */
+	public bFilter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+		HttpSession session = httpRequest.getSession();
+		String CAS_ST = httpRequest.getParameter(Constants.CAS_ST);
+		System.out.println("F2");
+		if (CAS_ST != null) {
+			// CAS返回ST
+			System.out.println("CAS_ST != null");
+			session.setAttribute(Constants.LOCAL_ST, CAS_ST);
+			String LOCAL_SERVICE = httpRequest
+					.getParameter(Constants.LOCAL_SERVICE);
+			if (LOCAL_SERVICE != null && !LOCAL_SERVICE.equals("")) {
+				System.out.println("LOCAL_SERVICE != null");
+				System.out.println(LOCAL_SERVICE);
+				httpResponse.sendRedirect(LOCAL_SERVICE);
+			}
+			else
+				httpResponse.sendRedirect(httpRequest.getContextPath());
+			return;
+		} else {
+			System.out.println("CAS_ST == null");
+			String LOCAL_ST = (String) session.getAttribute(Constants.LOCAL_ST);
+			if (LOCAL_ST == null) {
+				// 跳转到CAS登录
+				System.out.println("LOCAL_ST == null");
+				
+				httpResponse.sendRedirect(CAS_LOGIN_URL + "?"
+						+ Constants.LOCAL_SERVICE + "="
+						+ httpRequest.getRequestURL());
+			} else {
+				System.out.println("LOCAL_ST != null");
+				String LOCAL_USER_ID = (String) session
+						.getAttribute(Constants.LOCAL_USER_ID);
+				if (LOCAL_USER_ID == null) {
+					// 获取LOCAL_USER_ID
+					System.out.println("LOCAL_USER_ID == null");
+					try {
+						URL url = new URL(CAS_USER_URL + "?" + Constants.CAS_ST
+								+ "=" + LOCAL_ST + "&host="
+								+ httpRequest.getServerName() + "&app="
+								+ httpRequest.getContextPath() + "&"
+								+ Constants.LOCAL_SERVICE + "="
+								+ httpRequest.getRequestURL() + "&sessionId="
+								+ session.getId());
+						System.out.println(url);
+						BufferedReader reader = new BufferedReader(
+								new InputStreamReader(url.openStream()));
+						LOCAL_USER_ID = reader.readLine();
+						reader.close();
+						session.setAttribute(Constants.LOCAL_USER_ID,
+								LOCAL_USER_ID);
+					} catch (Exception e) {
+						e.printStackTrace();
+						// 跳转到CAS登录
+						httpResponse.sendRedirect(CAS_LOGIN_URL + "?"
+								+ Constants.LOCAL_SERVICE + "="
+								+ httpRequest.getRequestURL());
+					}
+
+				}
+				chain.doFilter(request, response);
+
+			}
+		}
+
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+//		CAS_LOGIN_URL = fConfig.getInitParameter(Constants.CAS_LOGIN_URL);
+//		CAS_USER_URL = fConfig.getInitParameter(Constants.CAS_USER_URL);
+	}
+
+}
---
+++ sso/cas/src/main/java/controllers/LogoutController.java
@@ -0,0 +1,65 @@
+package controllers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import cas.server.Constants;
+import database.DB;
+import domains.SessionStorage;
+@WebServlet(value="/logout.do")
+public class LogoutController extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("这里是cas的logout");
+		HttpSession session=request.getSession();
+		session.invalidate();
+		Cookie cookies[] = request.getCookies();
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				System.out.println(cookie.getName());
+				if (cookie.getName().equals(Constants.CAS_TGS)) {
+					String CAS_TGS = cookie.getValue();
+					String CAS_ST = CAS_TGS;
+					cookie.setMaxAge(0);
+					
+					response.addCookie(cookie);
+					List<SessionStorage> list =DB.findSessionStorage(CAS_ST);
+							
+					try {
+						for (SessionStorage item : list) {
+							URL url = new URL(item.getLocalService()
+									+ "?logout=true&sessionId="
+									+ item.getSessionId());
+							BufferedReader reader = new BufferedReader(
+									new InputStreamReader(url.openStream()));
+							reader.readLine();
+							reader.close();
+							System.out.println(url.toString());
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+
+					}
+					DB.deleteSessionStorage(CAS_ST);
+					DB.deleteServiceTicket(CAS_ST);
+				}
+			}
+		}
+		response.sendRedirect(request.getContextPath()+"/login.do");
+	}
+
+}
